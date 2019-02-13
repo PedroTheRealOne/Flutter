a117454c0d2bfd86c33db90180b27a287ec79750
Pedro <pedrotherealone1@gmail.co>
2019-02-13 15:25:34 -0200

    Improve
    Add SnackBar and remove option

--- tasklist/lib/main.dart
+++ tasklist/lib/main.dart
@@ -18,6 +18,20 @@ class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
   
   List _toDoList = [];
+  
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  @override
+  void initState(){
+    super.initState();
+
+    _readData().then((data){
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
 
   void _addToDo(){
     setState(() {
@@ -65,21 +79,76 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
               padding: EdgeInsets.only(top: 10.0),
               itemCount: _toDoList.length,
-              itemBuilder: (context, index){
-                return CheckboxListTile(
-                  title: Text(_toDoList[index]["title"]),
-                  value: _toDoList[index]["ok"],
-                  secondary: CircleAvatar(
-                    child: Icon(_toDoList[index]["ok"] ? Icons.check: Icons.error),
-                  ),
-                );
-              }),
+              itemBuilder: buildItem
+              ),
           )
         ],
       ),
 
     );
   }
+
+  Widget buildItem(context, index){
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white,),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+      title: Text(_toDoList[index]["title"]),
+      value: _toDoList[index]["ok"],
+      secondary: CircleAvatar(
+        child: Icon(_toDoList[index]["ok"] ? Icons.check: Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+    ),
+    onDismissed: (direction) {
+      setState(() {
+      _lastRemoved = Map.from(_toDoList[index]); 
+      _lastRemovedPos = index;
+      _toDoList.removeAt(index);
+      _saveData();
+
+      final snack = SnackBar(
+        content: Text("Task ${_lastRemoved["title"]} removed!"),
+        action: SnackBarAction(label: "Undo",
+        onPressed: () {
+          setState(() {
+          _toDoList.insert(_lastRemovedPos, _lastRemoved);
+          _saveData(); 
+          });
+        },),
+        duration: Duration(seconds: 2),
+      );
+      Scaffold.of(context).showSnackBar(snack);
+      });
+    },
+    );
+  }
+
+  /*CheckboxListTile(
+      title: Text(_toDoList[index]["title"]),
+      value: _toDoList[index]["ok"],
+      secondary: CircleAvatar(
+        child: Icon(_toDoList[index]["ok"] ? Icons.check: Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+    )*/
 
   Future<File> _getFile() async {
   final directory = await getApplicationDocumentsDirectory();
