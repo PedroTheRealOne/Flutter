6d98196df6b959efba85366ebf2c2f253b442004
Pedro <pedrotherealone1@gmail.co>
2019-02-27 16:10:23 -0300

    Add Buttons to the drawer

--- clothing_store/lib/tiles/drawer_tile.dart
+++ clothing_store/lib/tiles/drawer_tile.dart
@@ -10,7 +10,31 @@ class DrawerTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      
+      color: Colors.transparent,
+      child: InkWell(
+        child: Container(
+          height: 60.0,
+          child: Row(
+            children: <Widget>[
+              Icon(
+                icon,
+                size: 32.0,
+                color: Colors.black,
+              ),
+              SizedBox(width: 32.0,),
+              Text(text,
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black
+              ),
+              ),
+            ],
+          ),
+        ),
+        onTap: (){
+          
+        },
+      ),
     );
   }
 }
