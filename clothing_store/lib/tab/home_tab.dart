6d5743cfa00552d1cb058b61c35a5061e5b19723
Pedro <pedrotherealone1@gmail.co>
2019-02-27 15:07:27 -0300

    Add Images to HomeScreen

--- clothing_store/lib/tab/home_tab.dart
+++ clothing_store/lib/tab/home_tab.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
@@ -31,6 +34,43 @@ class HomeTab extends StatelessWidget {
                 title: const Text("Newest"),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance.collection("home").orderBy("pos").getDocuments(),
+
+              builder: (context, snapshot){
+                if(!snapshot.hasData){
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                } else {
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map(
+                      (doc){
+                        return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                      }
+                    ).toList(),
+                    children: snapshot.data.documents.map(
+                      (doc){
+                        return FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage,
+                          image: doc.data["image"],
+                          fit: BoxFit.cover,
+                        );
+                      }
+                    ).toList(),
+                  );
+                }
+              },
             ),
           ],
         ),
