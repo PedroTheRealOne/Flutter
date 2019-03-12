ef03cc0f8af94b76b73a2ac5f5457da63758ac3d
Pedro <pedrotherealone1@gmail.co>
2019-03-12 15:07:00 -0300

    Add Product Tiles

---
+++ clothing_store/lib/screens/product_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:clothing_store/datas/product_data.dart';
+
+class ProductScreen extends StatefulWidget {
+
+  final ProductData product;
+
+  ProductScreen(this.product);
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+
+  final ProductData product;
+
+  _ProductScreenState(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
